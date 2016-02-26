db2146ed520a178b9f3cc4e611c279e19c6c5b56
Austin Maddox <amaddox@wps-inc.com>
2016-02-26 14:26:04 -0700

    Install the Amazon Simple Systems Manager (SSM) agent
    For the Amazon EC2 Run Command to manage EC2 instances remotely.

--- AWS/ECS/clusters/web-production-cluster/user-data.sh
+++ AWS/ECS/clusters/web-production-cluster/user-data.sh
@@ -9,6 +9,8 @@
 # Specify which ECS cluster to associate this instance with.
 echo ECS_CLUSTER=web-production-cluster >> /etc/ecs/ecs.config
 
+cd /tmp
+
 # Download pip.
 curl -O https://bootstrap.pypa.io/get-pip.py
 
@@ -20,3 +22,7 @@ python27 get-pip.py
 
 # Grab all configuration files from S3.
 su - ec2-user -c "aws s3 cp s3://web-settings/production /home/ec2-user/web-settings/production --recursive"
+
+# Install the Simple Systems Manager (SSM) for the Amazon EC2 Run Command to manage EC2 instances remotely.
+curl https://amazon-ssm-us-west-2.s3.amazonaws.com/latest/linux_amd64/amazon-ssm-agent.rpm -o amazon-ssm-agent.rpm
+yum install -y amazon-ssm-agent.rpm
