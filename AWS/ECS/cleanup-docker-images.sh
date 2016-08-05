e5376e4b0a8da416efcb156c70d3ba2c5bb99f77
Austin Maddox <amaddox@wps-inc.com>
2016-08-05 09:46:38 -0600

    Cleanup Docker images based on ECS Cluster instead of Auto Scaling Group

--- AWS/ECS/cleanup-docker-images.sh
+++ AWS/ECS/cleanup-docker-images.sh
@@ -6,20 +6,26 @@
 # By default, Docker caches images indefinitely. Cached images can be useful to reduce the time needed to launch new tasks: if the image is cached, the container can be started from the cache. If you have a lot of images that are rarely used, as is common in CI or development environments, then cleaning these out is a good idea.
 
 if [ $# -lt 2 ]; then
-    echo "Usage: cleanup-docker-images.sh <region> <auto-scaling-group-name> <username> <organization> <repository> <branch> <revision>"
-    echo "The first two arguments are required: region, auto-scaling-group"
+    echo "Usage: cleanup-docker-images.sh <region> <cluster> <username> <organization> <repository> <branch> <revision>"
+    echo "The first two arguments are required: region, cluster"
     exit 0
 fi
 
 REGION=$1
-AUTO_SCALING_GROUP_NAME=$2
+CLUSTER=$2
 USERNAME=$3
 ORGANIZATION=$4
 REPOSITORY=$5
 BRANCH=$6
 REVISION=$7
 
-INSTANCES=($(aws autoscaling describe-auto-scaling-groups --region=${REGION} --auto-scaling-group-name "${AUTO_SCALING_GROUP_NAME}" | grep InstanceId | cut -d'"' -f4 | tr '\n' ' '))
+# Get instances based on the Auto Scaling Group Name.
+#INSTANCES=($(aws autoscaling describe-auto-scaling-groups --region=${REGION} --auto-scaling-group-name "${AUTO_SCALING_GROUP_NAME}" | grep InstanceId | cut -d'"' -f4 | tr '\n' ' '))
+
+# Get instance based on the ECS Cluster Name.
+# Note: This command requires instances be tagged with `Cluster => name-of-cluster`.
+# Utilizes `jq` command-line JSON processor. (https://stedolan.github.io/jq)
+INSTANCES=($(aws ec2 describe-instances --region=${REGION} --filters "Name=tag:Cluster,Values=${CLUSTER}" "Name=instance-state-name,Values=running" --query "Reservations[].Instances[].InstanceId" | jq --raw-output .[]))
 
 for INSTANCE_ID in ${INSTANCES[*]}
 do
